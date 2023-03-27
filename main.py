04201334769210bdec7c53d1378101f62b02c4df
Miroslav Zeljković <miroslavzeljkovic@gmail.com>
2023-03-27 14:46:34 +0200

    zastita duzine 2

--- main.py
+++ main.py
@@ -168,6 +168,7 @@ def dodaj_u_bazu():
     db.session.commit()
 
 
+
     return redirect(url_for("home_prikaz_filmova", logged_in=current_user.is_authenticated))
     # Morao gornju liniju zbog prenosenja info o logovanju
     return redirect("/", logged_in=current_user.is_authenticated)
